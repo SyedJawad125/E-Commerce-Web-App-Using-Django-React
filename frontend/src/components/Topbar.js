7e0a4824fdd550d1cdddd717840363e76a3a7099
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-07-20 12:24:56 +0500

    Changes

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -1,52 +1,65 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignInAlt, faUserPlus, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { useContext} from 'react'
+import {Link} from 'react-router-dom'
+import { AuthCon } from '../context/AuthContext'
+
 
 // npm install @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons 
   // @fortawesome/react-fontawesome
 
 const Topbar = () => {
+  
+
+  const {logout} = useContext(AuthCon)
+
   return (
     
-    <div className="top-bar">
+    <div className="top-bar" >
     <p></p>
         {/* Your other top bar content here  */}
         <div className="top-bar d-flex justify-content-between align-items-center px-3">
             {/* <div className="d-flex align-items-center">
             <p className="mb-0"></p>
             </div> */}
-          <div className="d-flex align-items-center">
-            <div className="icon-box d-flex align-items-center mx-2">
-               <span><FontAwesomeIcon icon={faPhoneAlt} className="mr-2" />
-                    (123) 456-7890</span>
-            </div>
-            <div className="icon-box d-flex align-items-center mx-2">
-               <span><FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
-               Sign In</span>
-            </div>
-            <div className="icon-box d-flex align-items-center mx-2">
-               <span><FontAwesomeIcon icon={faUserPlus} className="mr-2" />
-               Sign Up</span>
-            </div>
+          <div className="d-flex align-items-center" >
+                <div className="icon-box d-flex align-items-center mx-2">
+                  <span><FontAwesomeIcon icon={faPhoneAlt}  className="mr-2" />
+                        (123) 456-7890</span>
+                </div>
+                  {
+              localStorage.getItem('token') ? <Link onClick={logout}>Sign Out</Link> : <Link to="/login">
+                <div className="icon-box d-flex align-items-center mx-2">
+                    <span><FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
+                    Sign In</span>
+                </div></Link>
+             
+                   }
+                   
+                <div className="icon-box d-flex align-items-center mx-2">
+                  <span><FontAwesomeIcon icon={faUserPlus}  className="mr-2" />
+                  <Link to="/SignUp"  style={{ color: '#fff', marginLeft: 'auto' }}>SignUp</Link>
+                        </span>
+                </div>
           </div>
         </div>
-    </div>
-    // <div className="top-bar d-flex justify-content-between align-items-center px-3">
-    //   <div className="d-flex align-items-center">
-    //     <FontAwesomeIcon icon={faPhone} className="mr-2" />
-    //     <p className="mb-0">Call us: (123) 456-7890</p>
-    //   </div>
-    //   <div className="d-flex">
-    //     <div className="d-flex align-items-center mx-3">
-    //       <FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
-    //       <p className="mb-0">Sign In</p>
-    //     </div>
-    //     <div className="d-flex align-items-center">
-    //       <FontAwesomeIcon icon={faUserPlus} className="mr-2" />
-    //       <p className="mb-0">Sign Up</p>
-    //     </div>
-    //   </div>
-    // </div>
+        </div>
+      
+        /* {
+        localStorage.getItem('token') ? <Link onClick={logout}>Sign Out</Link> : <Link to="/login">
+          <div className="icon-box d-flex align-items-center mx-2">
+              <span><FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
+              Sign In</span>
+          </div>
+          </Link>   
+        }
+          <div className="icon-box d-flex align-items-center mx-2">
+              <span><FontAwesomeIcon icon={faUserPlus} className="mr-2" />
+              <button  type="submit" class="right-button" onClick={Signupfunction} >SignUp</button>
+              </span>
+          </div> */
+    
   )
 }
 
