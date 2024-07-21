03fc6ab99ca11b4a52408a3f442048af6ac34f37
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-07-21 12:53:29 +0500

    Changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,17 @@
 // src/components/Footer.js
 import React from 'react';
+import logo from '../images/logo5.jpg'
+
 
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-top">
-                <div className="footer-section">
-                    <h2>Give Logo Here</h2>
+                <div className="footer-section-logo">
+                    <a href="/">
+                       <img src={logo} alt="Logo" className="navbar-logo" />
+                    </a>
                 </div>
                 <div className="footer-section">
                     <h2>SUPPORT</h2>
@@ -20,25 +24,25 @@ const Footer = () => {
                 <div className="footer-section">
                     <h2>Useful Links</h2>
                     <ul>
-                        <li><a href="/faq">FAQ</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About Us</a></li>
                         <li><a href="/contact">Contact Us</a></li>
-                        <li><a href="/returns">Returns</a></li>
                     </ul>
                 </div>
                 <div className="footer-section">
                     <h2>Our Services</h2>
                     <ul>
-                        <li><a href="/faq">FAQ</a></li>
-                        <li><a href="/contact">Contact Us</a></li>
-                        <li><a href="/returns">Returns</a></li>
+                        <li><a href="/publicproduct">Products</a></li>
+                        <li><a href="/publiccategory">Categories</a></li>
+                        <li><a href="/publiccategory">Categories</a></li>
                     </ul>
                 </div>
                 <div className="footer-section">
                     <h2>Contct Us</h2>
                     <ul>
-                        <li><a href="/faq">FAQ</a></li>
-                        <li><a href="/contact">Contact Us</a></li>
-                        <li><a href="/returns">Returns</a></li>
+                        <li><a href="/faq">DHA 2, Islamabad Pakistan.</a></li>
+                        <li><a href="/contact">(+92) 333 1906382</a></li>
+                        <li><a href="/returns">nicenick1992@gmail.com</a></li>
                     </ul>
                 </div>
                 {/* <div className="footer-section">
