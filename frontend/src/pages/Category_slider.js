ea66d4c1de86b9b522639f497ae86b02e76807ea
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-07-25 22:28:25 +0500

    Changes

--- frontend/src/pages/Category_slider.js
+++ frontend/src/pages/Category_slider.js
@@ -72,7 +72,7 @@ const Category_slider = () => {
                                     </a>
                                     <div className="card-body">
                                         <h5 className="card-title card-title-custom">{item.name}</h5>
-                                        <h5 className="card-text card-text-custom">Des: {item.description}</h5>
+                                        {/* <h5 className="card-text card-text-custom">Des: {item.description}</h5> */}
                                     </div>
                                 </div>
                             </div>
