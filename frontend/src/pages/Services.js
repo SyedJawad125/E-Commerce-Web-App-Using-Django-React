f41eac09f65cc07f66d3a2b3e702479e9a900f08
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-07-30 18:02:51 +0500

    Changes

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -4,6 +4,8 @@ import img2 from '../images/2.jpg'
 import Product_vertical_slider from './Category_vertical_slider';
 import Readmore from './Readmore';
 import './About_Services_CSS.css';
+import '../App.css'
+
 
 
 
@@ -53,53 +55,45 @@ const Services = () => {
         <h1 class="text-center mb-4">Our Services</h1>
         <div class="row">
           
-          <div class="col-md-3 mb-3">
-            <div class="card-about-service h-100">
-              <img src={img1} class="card-img-top-about-services"  alt="Service 1" />
-              <div className="card-body-about-service" style={{ height: '170px' }}>
-              
+        <div class="col-md-3 mb-3">
+            <div class="card-service h-100">
+              <img src={img1} class="card-img-top-services" alt="Service 1" />
+              <div className="card-body-service" style={{ height: '170px' }}>
                 <h5 class="card-title">E-Commerce</h5>
-                {/* <p class="card-text">Explore our wide range of vehicles. From family cars to luxury sedans, we offer the best options to suit your needs.</p> */}
-                {/* <button class="btn"></button> */}
-                <Readmore  text={text1} maxLength={100}  />
+                <Readmore text={text1} maxLength={200} />
               </div>
             </div>
-          </div>
-          
-          <div class="col-md-3 mb-3">
-            <div class="card-about-service h-100">
-              <img src={img2} class="card-img-top-about-services"  alt="Service 2" />
-              <div class="card-body-about-service" style={{ height: '170px' }}>
-                <h5 class="card-title">Categories</h5>
-                {/* <p class="card-text">Our expert technicians provide top-notch maintenance services to keep your vehicle running smoothly.</p> */}
-                {/* <button class="btn">Read More</button> */}
-                <Readmore  text={text2} maxLength={100} />
-              </div>
-            </div>
-          </div>
+        </div>
         
-          <div class="col-md-3 mb-3">
-            <div class="card-about-service h-100">
-              <img src={img1} class="card-img-top-about-services"  alt="Service 3" /> 
-              <div class="card-body-about-service" style={{ height: '170px' }}>
-                <h5 class="card-title">Products</h5>
-                {/* <p class="card-text">We offer 24/7 customer support to assist you with any inquiries and ensure a seamless experience.</p> */}
-                {/* <button class="btn">Read More</button> */}
-                <Readmore  text={text3} maxLength={100} />
+        <div class="col-md-3 mb-3">
+            <div class="card-service h-100">
+              <img src={img1} class="card-img-top-services" alt="Service 1" />
+              <div className="card-body-service" style={{ height: '170px' }}>
+                <h5 class="card-title">E-Commerce</h5>
+                <Readmore text={text2} maxLength={200} />
               </div>
             </div>
-          </div>
-          <div class="col-md-3 mb-3">
-            <div class="card-about-service h-100">
-              <img src={img2} class="card-img-top-about-services" alt="Service 3" /> 
-              <div class="card-body-about-service" style={{ height: '170px' }}>
-                <h5 class="card-title">Explore New Arrival</h5>
-                {/* <p class="card-text">We offer 24/7 customer support to assist you with any inquiries and ensure a seamless experience.</p> */}
-                {/* <button class="btn">Read More</button> */}
-                <Readmore  text={text4} maxLength={100} />
+        </div>
+        
+        <div class="col-md-3 mb-3">
+            <div class="card-service h-100">
+              <img src={img1} class="card-img-top-services" alt="Service 1" />
+              <div className="card-body-service" style={{ height: '170px' }}>
+                <h5 class="card-title">E-Commerce</h5>
+                <Readmore text={text3} maxLength={200} />
               </div>
             </div>
-          </div>   
+        </div>
+        
+        <div class="col-md-3 mb-3">
+            <div class="card-service h-100">
+              <img src={img1} class="card-img-top-services" alt="Service 1" />
+              <div className="card-body-service" style={{ height: '170px' }}>
+                <h5 class="card-title">E-Commerce</h5>
+                <Readmore text={text4} maxLength={200} />
+              </div>
+            </div>
+        </div>
         </div>
       </div>
    <Product_vertical_slider/>
